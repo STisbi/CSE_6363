f505d1259ec10b1b1a38d8721d7082be818c803b
Seth <STisbi@gmail.com>
2019-11-25 12:53:25 -0600

    Base files added for Project 3

---
+++ Project_3/K_Means.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+def k_means():
+    data = pd.read_csv("ATNTFaceImages400.txt")
+
+
+
+#######################################################
+#                        MAIN                         #
+#######################################################
+def main():
+    k_means()
+
+
+# The 0th argument is the file name
+if __name__ == "__main__":
+    main()
